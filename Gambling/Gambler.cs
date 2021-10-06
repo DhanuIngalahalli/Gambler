ede1d93292bb6d14192658dd01396addd6538479
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-10-05 22:42:59 -0700

    Adding code for win or loose

--- Gambling/Gambler.cs
+++ Gambling/Gambler.cs
@@ -8,5 +8,21 @@ namespace Gambling
     {
         public const int stake = 100;
         public const int bet = 1;
+        public void StartGame()
+        {
+            int game = stake;
+            Random random = new Random();
+            int res = random.Next(0, 2);
+            if (res == 0)
+            {
+                game--;
+
+            }
+            else
+            {
+                game++;
+            }
+            Console.WriteLine(game);
+        }
     }
 }
--- Gambling/Program.cs
+++ Gambling/Program.cs
@@ -4,9 +4,12 @@ namespace Gambling
 {
     class Gambler
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Welcome to gambler");
+            Gambler gmb = new Gambler();
+            gmb.StartGame();
+
 
         }
     }
