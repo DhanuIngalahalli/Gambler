d8eec5f38329fcc50a4aedf2988612819742b044
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-10-05 22:02:59 -0700

    welcome to gambler

---
+++ Gambling/Gambler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambling
+{
+    class Program
+    {
+        public const int stake = 100;
+        public const int bet = 1;
+    }
+}
---
+++ Gambling/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Gambling
+{
+    class Gambler
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to gambler");
+
+        }
+    }
+}
